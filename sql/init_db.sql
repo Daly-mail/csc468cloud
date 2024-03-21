54be69bcbcc83d9b572598a813cda1ce9df96822
Daly-mail <123392470+Daly-mail@users.noreply.github.com>
2024-03-21 08:29:27 -0400

    Update init_db.sql

--- sql/init_db.sql
+++ sql/init_db.sql
@@ -1,5 +1,20 @@
-Create database csc468;
-Use csc468;
-Create table if not exist cattle
-(name VARCHAR(255), breed VARCHAR(255), color VARCHAR(255));
-LOAD data INFILE var/lib/mysql-files/cattle.csv into table cattle;
+--Reference:
+--https://blog.devgenius.io/building-a-mysql-docker-image-with-preloaded-data-step-by-step-guide-6d03c79edecf
+
+CREATE DATABASE csc468;
+
+USE csc468;
+
+CREATE TABLE cattle
+(
+   types  VARCHAR(255) NOT NULL,
+   breed  VARCHAR(255) NOT NULL,
+   PRIMARY KEY (types)
+);
+
+LOAD DATA INFILE '/var/lib/mysql-files/cattle.csv'
+INTO TABLE cattle
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
